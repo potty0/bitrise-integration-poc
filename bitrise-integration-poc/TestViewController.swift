00d6c5f655e1147baf5ba9caf6ceb066833bc4d6
Peter Balint <potty0@gmail.com>
2020-03-30 16:56:37 +0200

    Content update

--- bitrise-integration-poc/TestViewController.swift
+++ bitrise-integration-poc/TestViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  bitrise-integration-poc
+//  Bitrise-poc
 //
 //  Created by Peter Balint on 2020. 03. 30..
 //  Copyright © 2020. Peter Balint. All rights reserved.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TestViewController: UIViewController {
+    @IBOutlet private weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func testButtonPressed(sender: UIButton) {
+        infoLabel.text = "Pressed!"
+    }
 }
 
